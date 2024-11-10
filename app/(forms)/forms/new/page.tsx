2f2335d201107c25cd1505b77eaadcded1a3e04b
Aditya <raghavbhai4545@gmail.com>
2024-11-10 16:09:36 +0530

    Update page.tsx

--- app/(forms)/forms/new/page.tsx
+++ app/(forms)/forms/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Card, CardContent } from '@/components/ui/card'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Button } from '@/components/ui/button'
