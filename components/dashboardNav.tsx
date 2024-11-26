12ae35d51b4e792f8a931c41e978401819346980
Aditya <77260113+Raghav-45@users.noreply.github.com>
2024-11-26 23:07:55 +0530

    Update dashboardNav.tsx

--- components/dashboardNav.tsx
+++ components/dashboardNav.tsx
@@ -2,7 +2,7 @@
 
 import React, { FC } from 'react'
 import Link from 'next/link'
-import { CheckCircle, FilePlus, FileText, Home } from 'lucide-react'
+import { CheckCircle, FileText, Home } from 'lucide-react'
 import {
   Tooltip,
   TooltipContent,
