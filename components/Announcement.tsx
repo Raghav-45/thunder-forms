6a7cea534fb50827471fd181b28fc6f2466cc457
Aditya <77260113+Raghav-45@users.noreply.github.com>
2024-11-27 20:20:15 +0530

    Update Announcement.tsx

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -8,16 +8,25 @@ import { Separator } from '@/components/ui/separator'
 interface AnnouncementProps {
   text?: string
   href?: string
+  withoutIcon?: boolean
 }
 
-export const Announcement: FC<AnnouncementProps> = ({ text, href }) => {
+export const Announcement: FC<AnnouncementProps> = ({
+  text,
+  href,
+  withoutIcon = false,
+}) => {
   return (
     <Link
       href={href ?? '/templates'}
       className="group inline-flex items-center rounded-full bg-muted px-3 py-1 text-sm font-medium"
     >
-      <LayoutPanelTopIcon className="h-4 w-4" />{' '}
-      <Separator className="mx-1 h-4" orientation="vertical" />{' '}
+      {!withoutIcon && (
+        <>
+          <LayoutPanelTopIcon className="h-4 w-4" />{' '}
+          <Separator className="mx-1 h-4" orientation="vertical" />{' '}
+        </>
+      )}
       <span>{text ?? 'Introducing New Feedback Template'}</span>
       <ArrowRightIcon className="ml-2 h-4 w-4 -translate-x-0.5 transition-transform group-hover:translate-x-0.5" />
     </Link>
