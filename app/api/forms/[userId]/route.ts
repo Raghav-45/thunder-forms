e74ddf3f1d7c35ac57950e7db7a628896fa7c877
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-01-21 19:37:49 +0530

    Update route.ts

--- app/api/forms/[userId]/route.ts
+++ app/api/forms/[userId]/route.ts
@@ -3,13 +3,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function GET(
-  request: Request
-  // { params }: { params: { userId: string } }
-) {
-  // const forms = await prisma.forms.findMany({
-  //   where: { userId: params.userId },
-  // })
+export async function GET() {
   const forms = await prisma.forms.findMany()
   return NextResponse.json(forms)
 }
