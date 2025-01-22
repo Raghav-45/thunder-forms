ff5138119b78cb30c7ac27a0ca2b9e0f93dfee98
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-01-22 22:12:09 +0530

    Update page.tsx

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -101,14 +101,14 @@ export default function Dashboard() {
   return (
     // <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+      <div className="grid grid-cols-2 gap-2 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
         <Card x-chunk="dashboard-01-chunk-0">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
             <DollarSign className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
+            <div className="text-2xl font-bold">$4,523.89</div>
             <p className="text-xs text-muted-foreground">
               +20.1% from last month
             </p>
@@ -116,9 +116,7 @@ export default function Dashboard() {
         </Card>
         <Card x-chunk="dashboard-01-chunk-1">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Total Forms Created
-            </CardTitle>
+            <CardTitle className="text-sm font-medium">Total Forms</CardTitle>
             <Users className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
