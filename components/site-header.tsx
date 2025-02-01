a6a0d75145412a87fdd5818e1ed1e0a11c4c28db
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-02-01 15:59:30 +0530

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -8,7 +8,7 @@ import { siteConfig } from '@/config/site'
 import { UserNav } from './user-nav'
 
 export function SiteHeader() {
-  const isLoggedIn = false
+  const isLoggedIn = true
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
