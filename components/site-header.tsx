7793b0742bb407ab685423489fda82417b66e182
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-02-01 15:39:27 +0530

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,10 +3,12 @@ import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { MainNav } from '@/components/mainNav'
 import { buttonVariants } from './ui/button'
-import { GithubIcon, TerminalIcon } from 'lucide-react'
+import { GithubIcon, TerminalIcon, UserCircle2Icon } from 'lucide-react'
 import { siteConfig } from '@/config/site'
+import { UserNav } from './user-nav'
 
 export function SiteHeader() {
+  const isLoggedIn = false
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
@@ -16,54 +18,57 @@ export function SiteHeader() {
             <CommandMenu />
           </div> */}
           <nav className="flex items-center">
-            <Link
-              href="/dashboard"
-              className={cn(
-                buttonVariants({
-                  variant: 'secondary',
-                  size: 'sm',
-                }),
-                'mr-1'
-              )}
-            >
-              <TerminalIcon className="h-4 w-4" />
-              Go to dashboard
-            </Link>
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: 'ghost',
-                  }),
-                  'w-9 px-0'
-                )}
-              >
-                <GithubIcon className="h-4 w-4" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            {/* <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: "ghost",
-                  }),
-                  "w-9 px-0"
-                )}
-              >
-                <Icons.twitter className="h-3 w-3 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link> */}
-            {/* <ModeToggle /> */}
+            {isLoggedIn ? (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={cn(
+                    buttonVariants({
+                      variant: 'secondary',
+                      size: 'sm',
+                    }),
+                    'mr-1'
+                  )}
+                >
+                  <TerminalIcon className="size-4" />
+                  Go to dashboard
+                </Link>
+                <UserNav />
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/auth"
+                  className={cn(
+                    buttonVariants({
+                      variant: 'secondary',
+                      size: 'sm',
+                    }),
+                    'mr-1'
+                  )}
+                >
+                  <UserCircle2Icon className="size-4" />
+                  Login
+                </Link>
+                <Link
+                  href={siteConfig.links.github}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div
+                    className={cn(
+                      buttonVariants({
+                        variant: 'ghost',
+                      }),
+                      'w-9 px-0'
+                    )}
+                  >
+                    <GithubIcon className="size-4" />
+                    <span className="sr-only">GitHub</span>
+                  </div>
+                </Link>
+              </>
+            )}
           </nav>
         </div>
       </div>
