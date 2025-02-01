130b390b0d1ffaa9ece5583386921c7051640a9c
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-02-01 13:23:53 +0530

    Update page.tsx

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next'
-import Image from 'next/image'
 import Link from 'next/link'
 
 import { cn } from '@/lib/utils'
@@ -15,25 +14,25 @@ export const metadata: Metadata = {
 
 export default function AuthenticationPage() {
   return (
-    <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="align-middle max-h-screen h-screen content-center bg-gradient-to-r from-blue-400 to-purple-700">
-        <div className="container bg-black rounded-3xl overflow-hidden relative hidden w-[1200px] aspect-video flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="align-middle max-h-screen h-screen content-center md:bg-gradient-to-r md:from-blue-400 md:to-purple-700">
+      <div className="md:container relative bg-black md:rounded-3xl overflow-hidden grid w-full md:w-[1200px] md:aspect-video flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="hidden md:flex h-full flex-col bg-muted p-10 bg-zinc-900 text-white dark:border-r">
+          <div className="flex items-center text-2xl font-medium align-center text-center">
+            <Icons.ThunderFormsLogo className="h-auto w-8 mr-2" />
+            Thunder Forms
+          </div>
+          <div className="mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg italic">
+                &ldquo;Thunder Forms has saved me countless hours of work and
+                helped me deliver stunning forms effortlessly. It became my
+                go-to tool for building forms quickly and efficiently.&rdquo;
+              </p>
+              <p className="text-sm">- Aditya</p>
+            </blockquote>
+          </div>
+        </div>
+        <div className="p-2 md:p-8">
           <Link
             href="/examples/authentication"
             className={cn(
@@ -43,56 +42,37 @@ export default function AuthenticationPage() {
           >
             Login
           </Link>
-          <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-            <div className="absolute inset-0 bg-zinc-900" />
-            <div className="relative z-20 flex items-center text-2xl font-medium align-center text-center">
-              <Icons.ThunderFormsLogo className="h-auto w-8 mr-2" />
-              Thunder Forms
-            </div>
-            <div className="relative z-20 mt-auto">
-              <blockquote className="space-y-2">
-                <p className="text-lg italic">
-                  &ldquo;Thunder Forms has saved me countless hours of work and
-                  helped me deliver stunning forms effortlessly. It became my
-                  go-to tool for building forms quickly and efficiently.&rdquo;
-                </p>
-                <footer className="text-sm">- Aditya</footer>
-              </blockquote>
-            </div>
-          </div>
-          <div className="lg:p-8">
-            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-              <div className="flex flex-col space-y-2 text-center">
-                <h1 className="text-2xl font-semibold tracking-tight">
-                  Create an account
-                </h1>
-                <p className="text-sm text-muted-foreground">
-                  Enter your email below to create your account
-                </p>
-              </div>
-              <UserAuthForm />
-              <p className="px-8 text-center text-sm text-muted-foreground">
-                By clicking continue, you agree to our
-                <br />
-                <Link
-                  href="/terms"
-                  className="underline underline-offset-4 hover:text-primary"
-                >
-                  Terms of Service
-                </Link>{' '}
-                and{' '}
-                <Link
-                  href="/privacy"
-                  className="underline underline-offset-4 hover:text-primary"
-                >
-                  Privacy Policy
-                </Link>
-                .
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Create an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to create your account
               </p>
             </div>
+            <UserAuthForm />
+            <p className="text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our
+              <br />
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
