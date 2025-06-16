c08fba2604f5d8ccd4d78ebf8d3dff4603e8a023
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-06-17 03:47:14 +0530

    Refactor FormsPage layout and remove redundant header from FormsPage component

--- app/(dashboard)/dashboard/forms/layout.tsx
+++ app/(dashboard)/dashboard/forms/layout.tsx
@@ -7,6 +7,19 @@ export default function FormsPageLayout({
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <div className="flex flex-col items-center justify-between gap-y-4 px-4 lg:px-6">
+            <div className="flex w-full justify-between items-center">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-3xl leading-none font-bold">
+                  Forms Management
+                </h1>
+                <p className="text-muted-foreground text-sm">
+                  Effortlessly create, organize, and track your forms in one
+                  place.
+                </p>
+              </div>
+            </div>
+          </div>
           {children}
         </div>
       </div>
--- app/(dashboard)/dashboard/forms/page.tsx
+++ app/(dashboard)/dashboard/forms/page.tsx
@@ -1,20 +1,6 @@
-import { DataTable } from "@/components/data-table";
-import { tableData } from "@/config/data";
+import { DataTable } from '@/components/data-table'
+import { tableData } from '@/config/data'
 
 export default function FormsPage() {
-  return (
-    <>
-    <div className="flex flex-col items-center justify-between gap-y-4 px-4 lg:px-6">
-      <div className="flex w-full justify-between items-center">
-        <div className="flex flex-col gap-2">
-          <h1 className="text-3xl leading-none font-bold">Forms Management</h1>
-          <p className="text-muted-foreground text-sm">
-            Effortlessly create, organize, and track your forms in one place.
-          </p>
-        </div>
-      </div>
-    </div>
-    <DataTable data={tableData} />
-    </>
-  )
+  return <DataTable data={tableData} />
 }
