ea01c0ace8d6c301a1dd65765dc9cfcf3237237f
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-06-17 13:38:35 +0530

    Update Home page layout and enhance component structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,69 @@
-import Image from "next/image";
+import { BrowserMockup } from '@/components/SafariMockup'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
+import { siteConfig } from '@/config/site'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex items-center justify-center bg-background">
+      <section>
+        <div className="relative container py-32">
+          <header className="mx-auto max-w-3xl text-center">
+            <h1 className="font-anton text-5xl font-semibold tracking-tight text-foreground md:text-7xl">
+              Thunder Forms <br /> Unleash the Power of Forms.
+            </h1>
+            <p className="my-7 max-w-3xl tracking-tight text-muted-foreground md:text-xl">
+              Thunder Forms is a next-gen form builder that combines speed,
+              flexibility, and powerful customization for all your data
+              collection needs.
+            </p>
+          </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <Badge
+            variant="outline"
+            className="mx-auto mt-10 flex w-fit cursor-pointer items-center justify-center rounded-full border py-1 pr-3 pl-2 font-normal transition-all ease-in-out hover:gap-3"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            <Avatar className="relative -mr-5 overflow-hidden rounded-full border md:size-10">
+              <AvatarImage
+                src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-2.webp"
+                alt=""
+              />
+              <AvatarFallback>AB</AvatarFallback>
+            </Avatar>
+            <Avatar className="relative -mr-5 overflow-hidden rounded-full border md:size-10">
+              <AvatarImage
+                src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-5.webp"
+                alt=""
+              />
+              <AvatarFallback>AB</AvatarFallback>
+            </Avatar>
+            <Avatar className="relative -mr-5 overflow-hidden rounded-full border md:size-10">
+              <AvatarImage
+                src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-6.webp"
+                alt=""
+              />
+              <AvatarFallback>AB</AvatarFallback>
+            </Avatar>
+            <p className="ml-6 tracking-tight capitalize md:text-lg">
+              {' '}
+              Trusted by <span className="font-bold text-foreground">
+                10k+
+              </span>{' '}
+              users.
+            </p>
+          </Badge>
+
+          <div className="relative mt-12 flex h-full w-full flex-col items-center justify-center">
+            <BrowserMockup
+              className="w-full"
+              url={siteConfig.url.replace('https://', '')}
+              DahboardUrlDesktop="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/dashboard/dashboard-1.png"
+              DahboardUrlMobile="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/dashboard/dashboard-mobile-1.png"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="absolute bottom-0 h-2/3 w-full bg-linear-to-b from-transparent to-background" />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
     </div>
-  );
+  )
 }
