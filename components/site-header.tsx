4089c5878afbf5b1e57c20e4c8326cc98f0b4e70
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-06-18 01:54:08 +0530

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,16 +1,61 @@
-import { Separator } from '@/components/ui/separator'
-import { SidebarTrigger } from '@/components/ui/sidebar'
+import { MainNav } from '@/components/mainNav'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { TerminalIcon, UserCircle2Icon } from 'lucide-react'
+import Link from 'next/link'
+// import { UserNav } from '@/components/user-nav'
 
-export function SiteHeader() {
+import { createClient } from '@/utils/supabase/server'
+
+export async function SiteHeader() {
+  const supabase = await createClient()
+  const { data } = await supabase.auth.getUser()
+  const isLoggedIn = data?.user
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
-        <Separator
-          orientation="vertical"
-          className="mx-2 data-[orientation=vertical]:h-4"
-        />
-        <h1 className="text-base font-medium">Documents</h1>
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <MainNav />
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+          {/* <div className="w-full flex-1 md:w-auto md:flex-none">
+            <CommandMenu />
+          </div> */}
+          <nav className="flex items-center space-x-2">
+            {isLoggedIn ? (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={cn(
+                    buttonVariants({
+                      variant: 'secondary',
+                      size: 'sm',
+                    }),
+                    'mr-1'
+                  )}
+                >
+                  <TerminalIcon className="size-4" />
+                  Go to dashboard
+                </Link>
+                {/* <UserNav isDashboard={false} /> */}
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/auth/login"
+                  className={cn(
+                    buttonVariants({
+                      variant: 'secondary',
+                      size: 'sm',
+                    }),
+                    'mr-1'
+                  )}
+                >
+                  <UserCircle2Icon className="size-4" />
+                  Login
+                </Link>
+              </>
+            )}
+          </nav>
+        </div>
       </div>
     </header>
   )
