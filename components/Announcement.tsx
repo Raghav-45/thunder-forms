ad613071cbbd27f128646a12d79276d6bc59093c
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-06-17 14:36:39 +0530

    Create Announcement.tsx

---
+++ components/Announcement.tsx
@@ -0,0 +1,34 @@
+import { IconArrowRight } from '@tabler/icons-react'
+import { LayoutPanelTopIcon } from 'lucide-react'
+import Link from 'next/link'
+import { FC } from 'react'
+
+import { Separator } from '@/components/ui/separator'
+
+interface AnnouncementProps {
+  text?: string
+  href?: string
+  withoutIcon?: boolean
+}
+
+export const Announcement: FC<AnnouncementProps> = ({
+  text,
+  href,
+  withoutIcon = false,
+}) => {
+  return (
+    <Link
+      href={href ?? '/templates'}
+      className="group inline-flex items-center rounded-full bg-muted px-3 py-1 text-sm font-medium"
+    >
+      {!withoutIcon && (
+        <>
+          <LayoutPanelTopIcon className="h-4 w-4" />{' '}
+          <Separator className="mx-1 h-4" orientation="vertical" />{' '}
+        </>
+      )}
+      <span>{text ?? 'Introducing New Feedback Template'}</span>
+      <IconArrowRight className="ml-2 h-4 w-4 -translate-x-0.5 transition-transform group-hover:translate-x-0.5" />
+    </Link>
+  )
+}
