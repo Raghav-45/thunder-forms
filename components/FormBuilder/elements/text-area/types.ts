cd57d483ac7e7386b0ed0dd3186cc74a76321594
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-06-21 21:38:14 +0530

    Update types.ts

--- components/FormBuilder/elements/text-area/types.ts
+++ components/FormBuilder/elements/text-area/types.ts
@@ -1,6 +1,7 @@
 import { BaseFieldConfig } from '@/components/FormBuilder/types/types'
 
 export interface TextAreaConfig extends BaseFieldConfig {
+  type: 'text-input'
   inputType?: 'text' | 'email' | 'password' | 'tel' | 'url'
   minLength?: number
   maxLength?: number
