ceafd344c79d9264430d737d5f731d9e6a6adb8e
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-06-22 22:47:53 +0530

    constants

---
+++ components/FormBuilder/constants/index.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_FORM_TITLE = 'Untitled Form'
+export const DEFAULT_FORM_DESCRIPTION = 'Lorem ipsum dolor sit amet'
+
+export const initialFormSettings = {
+  title: DEFAULT_FORM_TITLE,
+  description: DEFAULT_FORM_DESCRIPTION,
+  expiresAt: undefined,
+  maxSubmissions: undefined,
+  redirectUrl: undefined,
+}
