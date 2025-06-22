c73ff9890352735610e535235473c6215a404d96
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-06-22 15:05:22 +0530

    update

--- components/FormBuilder/utils/helperFunctions.ts
+++ components/FormBuilder/utils/helperFunctions.ts
@@ -2,30 +2,30 @@ import { FIELD_REGISTRY, FieldConfig } from '@/components/FormBuilder/elements'
 import { avaliableFieldsType } from '@/components/FormBuilder/types/types'
 
 // Get the component for a specific field type
-export const getFieldComponent = (fieldType: avaliableFieldsType) => {
-  const registry = FIELD_REGISTRY[fieldType]
+export const getFieldComponent = (uniqueIdentifier: avaliableFieldsType) => {
+  const registry = FIELD_REGISTRY[uniqueIdentifier]
   if (!registry) {
-    throw new Error(`Unknown field type: ${fieldType}`)
+    throw new Error(`Unknown field uniqueIdentifier: ${uniqueIdentifier}`)
   }
   return registry.component
 }
 
 // Get the editor for a specific field type
-export const getFieldEditor = (fieldType: avaliableFieldsType) => {
-  const registry = FIELD_REGISTRY[fieldType]
+export const getFieldEditor = (uniqueIdentifier: avaliableFieldsType) => {
+  const registry = FIELD_REGISTRY[uniqueIdentifier]
   if (!registry) {
-    throw new Error(`Unknown field type: ${fieldType}`)
+    throw new Error(`Unknown field uniqueIdentifier: ${uniqueIdentifier}`)
   }
   return registry.editor
 }
 
 // Create a default configuration for a specific field type
 export const createDefaultFieldConfig = (
-  fieldType: avaliableFieldsType
+  uniqueIdentifier: avaliableFieldsType
 ): FieldConfig => {
-  const registry = FIELD_REGISTRY[fieldType]
+  const registry = FIELD_REGISTRY[uniqueIdentifier]
   if (!registry) {
-    throw new Error(`Unknown field type: ${fieldType}`)
+    throw new Error(`Unknown field uniqueIdentifier: ${uniqueIdentifier}`)
   }
   return registry.defaultConfig()
 }
