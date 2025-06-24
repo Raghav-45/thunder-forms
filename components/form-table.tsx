84845f7596eb5cbd6e4e6a489603d0477aba3e65
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-06-25 00:00:13 +0530

    Update form-table.tsx

--- components/form-table.tsx
+++ components/form-table.tsx
@@ -741,7 +741,7 @@ function TableCellViewer({ item }: { item: z.infer<typeof schema> }) {
           </form> */}
         </div>
         <DrawerFooter>
-          <Button>Submit</Button>
+          {/* <Button>Submit</Button> */}
           <DrawerClose asChild>
             <Button variant="outline">Done</Button>
           </DrawerClose>
