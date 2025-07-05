2d0c54c1f3e9868f7f6b8786a5145e5168ece9ea
Aditya <77260113+Raghav-45@users.noreply.github.com>
2025-07-06 02:02:44 +0530

    Update form.ts

--- lib/validators/form.ts
+++ lib/validators/form.ts
@@ -1,12 +1,25 @@
 import { z } from 'zod'
 
 export const FormValidator = z.object({
-  formName: z.string().min(10, 'Title is required'),
-  formDescription: z.string().min(10, 'Title is required').optional(),
+  title: z.string().min(10, 'Title is required'),
+  description: z.string().optional(),
   formFields: z.unknown(), // Assuming formFields is a complex object, adjust as necessary
   maxSubmissions: z.number().int().min(1, 'Must be at least 1').optional(),
-  expiresAt: z.date().optional(),
+  expiresAt: z
+    .coerce
+    .date()
+    .min(new Date(), 'Expiration date must be in the future')
+    .optional(),
   redirectUrl: z.string().url('Must be a valid URL').optional(),
 })
 
 export type CreateFormPayload = z.infer<typeof FormValidator>
+
+// export const FormUpdateValidator = z.object({
+//   title: z.string().min(10, 'Title is required').optional(),
+//   description: z.string().min(10, 'Title is required').optional(),
+//   formFields: z.unknown(), // Assuming formFields is a complex object, adjust as necessary
+//   maxSubmissions: z.number().int().min(1, 'Must be at least 1').optional(),
+//   expiresAt: z.date().optional(),
+//   redirectUrl: z.string().url('Must be a valid URL').optional(),
+// })
