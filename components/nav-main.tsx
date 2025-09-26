57fd9faec25923d1d80bd1642ea260f3e07bf94c
aditya <realraghavaditya@gmail.com>
2025-09-26 20:31:02 +0530

    Add cursor pointer to New Form button in NavMain component

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -38,7 +38,7 @@ export function NavMain({
             <Link href="/dashboard/builder/new-form" className="w-full">
               <SidebarMenuButton
                 tooltip="Quick Create"
-                className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                className="bg-primary cursor-pointer text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
               >
                 <IconCirclePlusFilled />
                 <span>New Form</span>
