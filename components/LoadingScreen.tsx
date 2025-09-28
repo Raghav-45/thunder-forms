bffda56dcd475cdaaca682a9b1c6653d3197573f
aditya <realraghavaditya@gmail.com>
2025-09-28 23:36:12 +0530

    Refactor LoadingScreen: Remove unnecessary pathname checks and simplify rendering logic

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import { Progress } from '@/components/ui/progress'
-import { usePathname } from 'next/navigation'
 import { Icons } from '@/components/Icons'
+import { Progress } from '@/components/ui/progress'
+import { useEffect, useState } from 'react'
 
 export default function LoadingScreen() {
-  const pathname = usePathname()
   const [progress, setProgress] = useState(13)
   const [showProgress, setShowProgress] = useState(false)
 
@@ -31,22 +29,17 @@ export default function LoadingScreen() {
     }
   }, [])
 
-  if (pathname == '/dashboard' || pathname.startsWith('/dashboard/responses') || pathname.startsWith('/dashboard/forms')) {
-    return (
-      <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black">
-        <div className="text-3xl mb-6 font-semibold animate-pulse inline-flex align-center text-center">
-          <Icons.Logo className="h-auto w-8 mr-2" />
-          Thunder Forms
-        </div>
-
-        {showProgress && (
-          <div className="w-72 space-y-3">
-            <Progress value={progress} className="h-1.5 w-full" />
-          </div>
-        )}
+  return (
+    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black">
+      <div className="text-3xl mb-6 font-semibold animate-pulse inline-flex align-center text-center">
+        <Icons.Logo className="h-auto w-8 mr-2" />
+        Thunder Forms
       </div>
-    )
-  } else {
-    return null
-  }
+      {showProgress && (
+        <div className="w-72 space-y-3">
+          <Progress value={progress} className="h-1.5 w-full" />
+        </div>
+      )}
+    </div>
+  )
 }
