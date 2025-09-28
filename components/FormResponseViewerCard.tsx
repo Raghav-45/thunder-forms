a463d27cc0754ad203676f147a965b2d686cb0f7
aditya <realraghavaditya@gmail.com>
2025-09-28 20:40:53 +0530

    Refactor FormResponseViewerCard: Simplify header layout and enhance button styling

--- components/FormResponseViewerCard.tsx
+++ components/FormResponseViewerCard.tsx
@@ -1,6 +1,9 @@
+import { FC } from 'react'
+
 import { Button } from '@/components/ui/button'
 import {
   Card,
+  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
@@ -18,13 +21,8 @@ import {
   PaginationContent,
   PaginationItem,
 } from '@/components/ui/pagination'
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  Copy,
-  MoreVertical
-} from 'lucide-react'
-import { FC } from 'react'
+import { format } from 'date-fns'
+import { ChevronLeftIcon, ChevronRightIcon, MoreVertical } from 'lucide-react'
 
 const fakeResponse = {
   id: 'cmc94ml0v000bybkgit97n0e0',
@@ -53,31 +51,19 @@ const fakeResponse = {
 const FormResponseViewerCard: FC = ({}) => {
   return (
     <Card className="w-full max-w-2xl rounded-xl border shadow-lg bg-card">
-      <CardHeader className="flex flex-row items-start gap-4 border-b">
-        <div className="flex-1 space-y-1">
-          <CardTitle className="flex items-center gap-2 text-lg font-semibold">
-            Response Viewer
-          </CardTitle>
-          <CardDescription className="flex items-center gap-2 group text-xs">
-            {fakeResponse?.id}
-            <Button
-              size="icon"
-              variant="ghost"
-              className="size-5 opacity-0 group-hover:opacity-100 transition-opacity"
-            >
-              <Copy className="size-3" />
-              <span className="sr-only">Copy Response ID</span>
-            </Button>
-          </CardDescription>
-        </div>
-        <div className="flex items-center gap-1">
-          {/* <Button size="sm" variant="outline" className="h-8 gap-1">
-            <FileEditIcon className="h-4 w-4" />
-            <span className="sr-only lg:not-sr-only">Edit Response</span>
-          </Button> */}
+      <CardHeader className="border-b">
+        <CardTitle className="text-lg font-semibold">Response Viewer</CardTitle>
+        <CardDescription className="text-xs text-muted-foreground">
+          {fakeResponse?.id}
+        </CardDescription>
+        <CardAction>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button size="icon" variant="outline" className="h-8 w-8">
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shadow-none"
+              >
                 <MoreVertical className="h-4 w-4" />
                 <span className="sr-only">More</span>
               </Button>
@@ -86,7 +72,7 @@ const FormResponseViewerCard: FC = ({}) => {
               <DropdownMenuItem>Copy JSON</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-        </div>
+        </CardAction>
       </CardHeader>
       <CardContent>
         <div className="rounded-lg bg-neutral-950/60 px-4 py-3 overflow-x-auto">
@@ -96,40 +82,24 @@ const FormResponseViewerCard: FC = ({}) => {
         </div>
       </CardContent>
       <CardFooter className="flex flex-row items-center gap-4 border-t px-6">
-        {/* <div className="text-xs text-muted-foreground">
-          {selectedResponse?.createdAt &&
-            `Submitted on ${format(
-              new Date(selectedResponse.createdAt),
-              'PPP'
-            )} @ ${format(new Date(selectedResponse.createdAt), 'p')}`}
-        </div> */}
-        <Button variant="destructive" size="sm" className="h-7 text-xs">
-          Delete response
-        </Button>
+        <div className="text-xs text-muted-foreground">
+          {fakeResponse?.createdAt &&
+            `${format(new Date(fakeResponse.createdAt), 'PPP')} @ ${format(
+              new Date(fakeResponse.createdAt),
+              'p'
+            )}`}
+        </div>
         <div className="ml-auto flex items-center gap-2">
           <Pagination>
             <PaginationContent>
               <PaginationItem>
-                <Button
-                  size="icon"
-                  variant="outline"
-                  className="h-7 w-7"
-                  // disabled={!responses || selectedResponseIndex <= 0}
-                >
+                <Button size="icon" variant="outline" className="h-7 w-7">
                   <ChevronLeftIcon className="h-4 w-4" />
                   <span className="sr-only">Previous</span>
                 </Button>
               </PaginationItem>
               <PaginationItem>
-                <Button
-                  size="icon"
-                  variant="outline"
-                  className="h-7 w-7"
-                  // disabled={
-                  //   !responses ||
-                  //   selectedResponseIndex >= (responses?.length ?? 0) - 1
-                  // }
-                >
+                <Button size="icon" variant="outline" className="h-7 w-7">
                   <ChevronRightIcon className="h-4 w-4" />
                   <span className="sr-only">Next</span>
                 </Button>
