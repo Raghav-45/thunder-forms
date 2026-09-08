08d73b584f4443bf0c0140a2053ec048b1cbe878
Aditya <realraghavaditya@gmail.com>
2026-09-09 01:26:08 +0530

    feat: enhance DashboardSidebar with improved sidebar structure and dropdown menu functionality

--- containers/dashboard/components/sidebar/index.tsx
+++ containers/dashboard/components/sidebar/index.tsx
@@ -1,17 +1,160 @@
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from '@/components/ui/sidebar'
+import { cn } from '@/lib/utils'
+import {
+  IconCirclePlusFilled,
+  IconDots,
+  IconFolder,
+  IconMail,
+  IconShare3,
+  IconTrash,
+} from '@tabler/icons-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { FC } from 'react'
 import { sidebarLinks } from '../../constants'
-import { NavMain } from './nav-main'
-import { NavOther } from './nav-other'
-import { NavSecondary } from './nav-secondary'
 
 interface DashboardSidebarProps {}
 
 export const DashboardSidebar: FC<DashboardSidebarProps> = ({}) => {
+  const { isMobile } = useSidebar()
+  const pathname = usePathname()
+  const checkIsActive = (link: string) => {
+    const isActive = pathname === link
+    return isActive
+  }
   return (
     <>
-      <NavMain items={sidebarLinks.navMain} />
-      <NavSecondary items={sidebarLinks.navSecondary} />
-      <NavOther items={sidebarLinks.navOther} className="mt-auto" />
+      <SidebarGroup>
+        <SidebarGroupContent className="flex flex-col gap-2">
+          <SidebarMenu>
+            <SidebarMenuItem className="flex items-center gap-2">
+              <Link href="/dashboard/builder/new-form" className="w-full">
+                <SidebarMenuButton
+                  tooltip="Quick Create"
+                  className="bg-primary cursor-pointer text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                >
+                  <IconCirclePlusFilled />
+                  <span>New Form</span>
+                </SidebarMenuButton>
+              </Link>
+              <Button
+                size="icon"
+                className="size-8 group-data-[collapsible=icon]:opacity-0"
+                variant="outline"
+              >
+                <IconMail />
+                <span className="sr-only">Inbox</span>
+              </Button>
+            </SidebarMenuItem>
+          </SidebarMenu>
+          <SidebarMenu>
+            {sidebarLinks.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton tooltip={item.title} asChild>
+                  <Link
+                    href={item.url}
+                    className={cn(
+                      checkIsActive(item.url) ? 'bg-sidebar-accent' : '',
+                    )}
+                  >
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+      <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+        <SidebarGroupLabel>Repository</SidebarGroupLabel>
+        <SidebarMenu>
+          {sidebarLinks.navSecondary.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <a
+                  href={item.url}
+                  className={cn(
+                    checkIsActive(item.url) ? 'bg-sidebar-accent' : '',
+                  )}
+                >
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              </SidebarMenuButton>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuAction
+                    showOnHover
+                    className="data-[state=open]:bg-accent rounded-sm"
+                  >
+                    <IconDots />
+                    <span className="sr-only">More</span>
+                  </SidebarMenuAction>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  className="w-24 rounded-lg"
+                  side={isMobile ? 'bottom' : 'right'}
+                  align={isMobile ? 'end' : 'start'}
+                >
+                  <DropdownMenuItem>
+                    <IconFolder />
+                    <span>Open</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <IconShare3 />
+                    <span>Share</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem variant="destructive">
+                    <IconTrash />
+                    <span>Delete</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          ))}
+          {/* <SidebarMenuItem>
+          <SidebarMenuButton className="text-sidebar-foreground/70">
+            <IconDots className="text-sidebar-foreground/70" />
+            <span>More</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem> */}
+        </SidebarMenu>
+      </SidebarGroup>
+      <SidebarGroup className="mt-auto">
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {sidebarLinks.navOther.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
     </>
   )
 }
