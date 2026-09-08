55b2c52e380a8848cd9e74c2f213b88881667d0e
Aditya <realraghavaditya@gmail.com>
2026-09-09 03:21:06 +0530

    feat: refactor FormsPage and TemplatePage to use DashboardLayout for consistency

--- containers/dashboard/templates/index.tsx
+++ containers/dashboard/templates/index.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react'
-import Layout from '../layout'
+import { DashboardLayout } from '../dashboard-layout'
 import { TemplateGallery } from './components/template-gallery'
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
   return (
-    <Layout>
+    <DashboardLayout>
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
@@ -28,7 +28,7 @@ const page: FC<pageProps> = ({}) => {
           </div>
         </div>
       </div>
-    </Layout>
+    </DashboardLayout>
   )
 }
 
