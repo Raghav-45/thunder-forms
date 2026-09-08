dee1e09ceebc74249a6409fe483e3075189807b6
Aditya <realraghavaditya@gmail.com>
2026-09-09 00:47:57 +0530

    refactor: remove unused 'use client' directive and delete TemplateGallery component

--- app/(dashboard)/dashboard/templates/page.tsx
+++ app/(dashboard)/dashboard/templates/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Default from '@/containers/dashboard/templates'
 
 const page = ({}) => {
--- containers/dashboard/forms/index.tsx
+++ containers/dashboard/forms/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   FormTable,
   schema as TableDataItemSchema,
